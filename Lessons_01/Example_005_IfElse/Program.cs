0049e0857415439256e4d057658d976a918fedba
Marat <msshayapov@gmail.com>
2022-11-29 22:14:47 +0600

    Example 005 If-Else Приветствие особенного пользователя

---
+++ Lessons_01/Example_005_IfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿// В данной программе будем использовать оператор If / Else 
+
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine("Ура! Это же Наташа!");
+}
+else
+    if(username.ToLower() == "марат")
+{
+    Console.WriteLine("Ура! Это же Марат!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);   
+}
