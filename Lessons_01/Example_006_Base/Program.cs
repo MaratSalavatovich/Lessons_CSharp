e63a0ef92b784bc382275afc4781bdd738f10710
Marat <msshayapov@gmail.com>
2022-11-29 22:27:24 +0600

    Example 006 Base

---
+++ Lessons_01/Example_006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿// Решаем про гири
+int a = 3;
+int b = 7;
+int c = 5;
+int d = 3;
+int e = 1;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write ("Значение max = ");
+Console.WriteLine(max);
