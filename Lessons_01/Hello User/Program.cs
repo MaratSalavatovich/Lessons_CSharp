b9f1e7bb8b1a09a465749cec9e71b830f5446c88
Marat <msshayapov@gmail.com>
2022-11-29 18:04:10 +0600

    CSharp Lessons 01 Hello User created

---
+++ Lessons_01/Hello User/Program.cs
@@ -0,0 +1,5 @@
+﻿// Пропишем самую простую программу - поприветствовать User!
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();  
+Console.Write("Привет, ");
+Console.Write(username);
