4eb51175ec624a75c31353dab2d196b3f24eca24
Marat <msshayapov@gmail.com>
2022-12-01 18:40:19 +0600

    Решили первую задачу Вывести квадрат числа

---
+++ tasks/Ex_01/Program.cs
@@ -0,0 +1,6 @@
+﻿// 0. Вывести квадрат числа
+Console.WriteLine("start");
+int number = 23; // int - целое число, определили целое число, задали сами
+int result = number * number; // вычисление квадратного числа
+Console.WriteLine(result); // вывод вычисления
+Console.WriteLine("end");
