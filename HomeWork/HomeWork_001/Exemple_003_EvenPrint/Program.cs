c6dbef9e49ccf7a6a47dcd44dd000e39793cddae
Marat <msshayapov@gmail.com>
2022-12-03 02:02:15 +0600

    Решение 3 задачи которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

---
+++ HomeWork/HomeWork_001/Exemple_003_EvenPrint/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая 
+//на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите натуральное число :"); 
+int N = Convert.ToInt32(Console.ReadLine()); 
+if (N >= 2) {
+  for (int i = 2; i <= N; i +=2) 
+  {
+      Console.WriteLine(i);
+  }
+}
+else 
+{
+    Console.WriteLine("N < 2");
+};
+
