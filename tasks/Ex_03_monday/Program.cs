ccbf09c1c3ad5e8c70b3204d262221e91e4d03a7
Marat <msshayapov@gmail.com>
2022-12-20 21:19:20 +0600

    решение задачи с индексом 3

---
+++ tasks/Ex_03_monday/Program.cs
@@ -0,0 +1,20 @@
+﻿// 3. По заданному номеру дня недели вывести его название
+
+Console.Write("Введите день недели: ");
+int dayNumber = int.Parse(Console.ReadLine());
+if (dayNumber == 1)
+    Console.WriteLine("Сегодня понедельник");
+if (dayNumber == 2)
+    Console.WriteLine("Сегодня вторник");
+if (dayNumber == 3)
+    Console.WriteLine("Сегодня среда.");
+if (dayNumber == 4)
+    Console.WriteLine("Сегодня черверг.");
+if (dayNumber == 5)
+    Console.WriteLine("Сегодня пятница.");
+if (dayNumber == 6)
+    Console.WriteLine("Сегодня суббота.");
+if (dayNumber == 7)
+    Console.WriteLine("Сегодня воскресенье.");
+else if (dayNumber < 1 || dayNumber > 7)
+    Console.WriteLine(" Введено не корректное число ");
