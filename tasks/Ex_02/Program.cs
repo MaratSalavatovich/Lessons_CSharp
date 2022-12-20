6f3d9c1cd0d2397587081736685c716bc883f75c
Marat <msshayapov@gmail.com>
2022-12-20 21:34:31 +0600

    решение задачи с индексом 2 - маскимальное из введеных чисел

---
+++ tasks/Ex_02/Program.cs
@@ -0,0 +1,6 @@
+﻿//2. Даны два числа. Показать большее и меньшее число
+int number1 = 1;
+int number2 = 2;
+int max = number1;
+if (max < number2) max = number2;
+Console.WriteLine($"Масимальное число: {max}");
