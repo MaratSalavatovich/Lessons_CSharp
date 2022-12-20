e523a454065d91606a525102144bd6c9566798e4
Marat <msshayapov@gmail.com>
2022-12-20 21:29:24 +0600

    решение задачи с индексом 4 - маскимальное из введеных чисел пользователем

---
+++ tasks/Ex_04_max/Program.cs
@@ -0,0 +1,16 @@
+﻿//4. Найти максимальное из трех чисел
+
+//ввод чисел пользователем
+Console.Write("Введите число №1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число №3: ");
+int number3 = int.Parse(Console.ReadLine());
+// метод сравнения чисел
+int max = number1;
+if (max < number2) max = number2;
+if (max < number3) max = number3;
+// вывод в консоль
+Console.WriteLine($"Максимальное число: {max}");
+
