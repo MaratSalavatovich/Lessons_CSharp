36b7f0a53bdcffd58bf40fdafa585fef89f62d86
Marat <msshayapov@gmail.com>
2023-01-12 10:34:54 +0100

    решение домашнего задания 9

---
+++ HomeWork/HomeWork_009/Ex_01/Program.cs
@@ -0,0 +1,12 @@
+﻿string Number(int N, int b)
+
+
+{
+    //string result = string.Empty 
+
+    if (N >= b) return $"{N} " + Number(N - 1, b);
+    else return String.Empty;
+
+
+}
+Console.WriteLine(Number(8, 1));
---
+++ HomeWork/HomeWork_009/Ex_02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int Number(int M, int N)
+{
+
+
+    if (M == N) return M;
+    else return M + Number(M + 1, N);
+
+
+}
+Console.WriteLine(Number(1, 15));
